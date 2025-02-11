e709d9f81532d343382402de2d3b91f386616ff8
matty87a <matty87a@gmail.com>
2025-02-11 16:13:53 +0000

    Squash

--- s3/kms.tf
+++ s3/kms.tf
@@ -3,6 +3,7 @@ resource "aws_kms_key" "objects" {
   enable_key_rotation     = true
   rotation_period_in_days = 90
   deletion_window_in_days = 30
+  policy                  = data.aws_iam_policy_document.kms_policy[each.key].json
 }
 
 resource "aws_kms_alias" "objects" {
